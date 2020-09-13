0791df8ec932e1b52dcf17a31780d6e96fec3d62
Megan Leich <51799170+maleich@users.noreply.github.com>
2020-09-13 15:35:55 -0700

    Create call_functions_edit.py

---
+++ call_functions_edit.py
@@ -0,0 +1,18 @@
+# Rearrange the function calls so they make logical sense.
+
+def say_hi():
+   print("Why hello there!")
+
+def offer_drink():
+   print("Would you care for a spot of tea?")
+
+def offer_food():
+   print("Biscuit?")
+
+def say_bye():
+   print("Cheerio then.")
+
+
+offer_drink()
+say_hi()
+offer_food()
